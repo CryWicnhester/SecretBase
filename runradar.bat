703e40c5163d41133ca767b58cd10317e48f95ea
liantian <liantianme@gmail.com>
2018-03-25 23:06:53 +0800

    Init

---
+++ runradar.bat
@@ -0,0 +1,4 @@
+
+set arg1=%1
+java -jar %arg1% 192.168.1.53 PortFilter 192.168.1.53
+
---
+++ src/main/kotlin/pubg/radar/struct/Bunch.kt
@@ -0,0 +1,84 @@
+package pubg.radar.struct
+
+import com.badlogic.gdx.math.Quaternion
+import com.badlogic.gdx.math.Vector3
+import pubg.radar.deserializer.Buffer
+
+class Bunch(
+        private val BunchDataBits: Int,
+        buffer: Buffer,
+        private val PacketID: Int,
+        private val ChIndex: Int,
+        private val ChType: Int,
+        var ChSequence: Int,
+        val bOpen: Boolean,
+        var bClose: Boolean,
+        var bDormant: Boolean,
+        var bIsReplicationPaused: Boolean,
+        val bReliable: Boolean,
+        val bPhantomBit: Boolean,
+        val bPartial: Boolean,
+        val bPartialInitial: Boolean,
+        var bPartialFinal: Boolean,
+        val bHasPackageMapExports: Boolean,
+        var bHasMustBeMappedGUIDs: Boolean
+) : Buffer(buffer) {
+
+    override fun deepCopy(copyBits: Int): Bunch {
+        val buf = super.deepCopy(copyBits)
+        return Bunch(
+                BunchDataBits,
+                buf,
+                PacketID,
+                ChIndex,
+                ChType,
+                ChSequence,
+                bOpen,
+                bClose,
+                bDormant,
+                bIsReplicationPaused,
+                bReliable,
+                bPhantomBit,
+                bPartial,
+                bPartialInitial,
+                bPartialFinal,
+                bHasPackageMapExports,
+                bHasMustBeMappedGUIDs
+        )
+    }
+
+    var next: Bunch? = null
+    fun propertyBool() = readBit()
+    fun propertyFloat() = readFloat()
+    fun propertyInt() = readInt32()
+    fun propertyByte() = readByte()
+    fun propertyName() = readName()
+    fun propertyObject() = readObject()
+    fun propertyUInt32() = readUInt32()
+    fun propertyVector() = Vector3(readFloat() , readFloat() , readFloat())
+    fun propertyRotator() = Vector3(readFloat() , readFloat() , readFloat())
+    fun propertyPlane() = Quaternion(propertyVector() , readFloat())
+    fun propertyVector100() = readVector(100 , 30)
+    fun propertyNetId() = if (readInt32() > 0) readString() else ""
+    fun propertyVectorNormal() = readFixedVector(1 , 16)
+    fun propertyVector10() = readVector(10 , 24)
+    fun propertyVectorQ() = readVector(1 , 20)
+    fun propertyString() = readString()
+    fun propertyUInt64() = readInt64()
+}
+
+fun Bunch.repMovement(actor: Actor) {
+    val bSimulatedPhysicSleep = readBit()
+    val bRepPhysics = readBit()
+    actor.location = if (actor.isAPawn)
+        readVector(100, 30)
+    else readVector(1, 24)
+
+    actor.rotation = if (actor.isACharacter)
+        readRotationShort()
+    else readRotation()
+
+    actor.velocity = readVector(1, 24)
+    if (bRepPhysics)
+        readVector(1, 24)
+}
