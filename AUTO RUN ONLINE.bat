89c8367d9f858c8129620de993e7ef6b10f088a3
liantian <liantianme@gmail.com>
2018-03-26 21:53:53 +0800

    del readme

--- AUTO RUN ONLINE.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-for /f "tokens=14" %%a in ('ipconfig ^| findstr IPv4') do set _IPaddr=%%a
-echo YOUR IP ADDRESS IS: %_IPaddr%
-echo "RUNNING PUBG-RADAR"
-set /p game=ENTER GAME PC IP:
-echo "%game%"
- archive\RadarProject.exe %_IPaddr% PortFilter %game%
-
-pause
